56d4613c77b1710bc7448fa433a0a63e7de1a4d9
Paul Steven Malvar <56985380+malvarpaulsteven@users.noreply.github.com>
2023-01-10 15:17:11 +0800

    updated design

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>here-me-out</title>
+    <title>HereMeOut</title>
+    <link rel="shortcut icon" href="{{ URL('img/mental2.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-warning">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm ">
+        <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm  fixed-top">
             <div class="container">
                 <a class="navbar-brand text-light" href="{{ url('/') }}">
                    Here.Me.Out
@@ -45,8 +46,17 @@
                                 </li>
                             @endif
                         @else
-                                <a href="{{ url('/') }}" class="nav-link text-light">Home</a>
-                                <a href="{{ url('/dashboard') }}" class="nav-link text-light">Dashboard</a>
+                        <li class="nav-item d-flex flex-direction-row">
+                            <a href="{{ url('/') }}" class="nav-link text-light">Home</a>
+                        </li>
+                        <li class="nav-item d-flex flex-direction-row">
+                            <a href="{{ url('/dashboard') }}" class="nav-link text-light">Dashboard</a>
+                        </li>
+                        <li class="nav-item d-flex flex-direction-row">
+                            <a href="{{ url('/') }}" class="nav-link text-light">About Us</a>
+                        </li>
+
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
